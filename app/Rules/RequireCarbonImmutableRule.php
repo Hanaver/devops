6fcaa1592139b38127dd59f6480b0adddd9dce75
Andy <746157024@qq.com>
2026-05-21 20:34:00 +0800

    增加CI自动测试项目工作

--- app/Rules/RequireCarbonImmutableRule.php
+++ app/Rules/RequireCarbonImmutableRule.php
@@ -8,6 +8,11 @@ use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
 
+/**
+ * 重点在这里：添加泛型注释，声明当前 Rule 绑定的具体节点类型
+ *
+ * @implements Rule<New_>
+ */
 class RequireCarbonImmutableRule implements Rule
 {
     public function getNodeType(): string
@@ -15,6 +20,11 @@ class RequireCarbonImmutableRule implements Rule
         return New_::class;
     }
 
+    /**
+     * 有了上面的类注释，PHPStan 在分析这里时，
+     * 就会自动知道传进来的 $node 实际上是 PhpParser\Node\Expr\New_ 对象，
+     * 因此它不会再因为找不到 $class 属性而报错了。
+     */
     public function processNode(Node $node, Scope $scope): array
     {
         if(!$node->class instanceof Node\Name){
